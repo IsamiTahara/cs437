ec7fb073ed085186b993981977916c3c29489b7d
csVoidMonarch <123682535+csVoidMonarch@users.noreply.github.com>
2023-01-26 15:25:08 -0600

    Update Volume.cpp

--- Volume.cpp
+++ Volume.cpp
@@ -17,9 +17,13 @@ int main() {
    waterVolume = volume * 7.48;
    time = waterVolume / (rate * 3.78541 / 60);
 
+   //Set precision for volume and water volume to 2 decimal places
    cout << fixed << setprecision(2);
    cout << "Reservoir volume: " << volume << " cu. ft" << endl;
    cout << "Water volume: " << waterVolume << " gallons" << endl;
+
+   //Set precision for time to fill to 1 decimal place
+   cout << fixed << setprecision(1);
    cout << "Time to fill: " << time << " hour(s)" << endl;
 
    return 0;
